e48b97717bb49d7ad8cd153ab9c8a649d4f072c0
Mackay89 <manotomackay@gmail.com>
2024-08-05 14:18:36 -0700

    task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ This module contains the asynchronous coroutine wait_n.
 
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for task_wait_random function"""
 import asyncio
-from basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns an asyncio.Task for wait_random with max_delay."""
